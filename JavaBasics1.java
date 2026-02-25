daa274ebb64dc9b169540989974ed34ff4df67c7
Arti <artisoni12.10.2003@gmail.com>
2026-02-25 14:03:50 +0530

    output in Java

--- JavaBasics1.java
+++ JavaBasics1.java
@@ -1,6 +1,7 @@
 public class JavaBasics1 {
     public static void main (String args[]){
-        System.out.println("helloo");
+        System.out.print("helloo\narti\n");
+        System.out.println("1, 2, 3, 4, 5");
     }
 }
 
