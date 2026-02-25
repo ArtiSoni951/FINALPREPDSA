d4d17cba08ba7029ba8f65d6eb4761698c2096e6
Arti <artisoni12.10.2003@gmail.com>
2026-02-25 14:08:18 +0530

    print pattern

--- JavaBasics1.java
+++ JavaBasics1.java
@@ -1,7 +1,20 @@
 public class JavaBasics1 {
     public static void main (String args[]){
-        System.out.print("helloo\narti\n");
-        System.out.println("1, 2, 3, 4, 5");
+
+        //output in java
+
+        // System.out.print("helloo\narti\n");
+        // System.out.println("1, 2, 3, 4, 5");
+
+        //print star pattern
+
+        System.out.println("****");
+        System.out.println("***");
+        System.out.println("**");
+        System.out.println("*");
     }
 }
+
+
+
 
